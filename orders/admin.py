426d44ffbf9d267e88edaa615e70aa43d89acb3a
Dima <dimaps716@gmail.com>
2021-05-21 19:15:29 -0500

    creation of the orders app and its models

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+	model = OrderItem
+	raw_id_fields = ['product']
+
+
+class OrderAdmin(admin.ModelAdmin):
+	list_display = ['id','costumer_name','costumer_email','costumer_mobile','status', 'created_at', 'updated_at']
+	list_filter = ['paid', 'created_at', 'updated_at']
+	inlines = [OrderItemInline]
+
+
+admin.site.register(Order, OrderAdmin)

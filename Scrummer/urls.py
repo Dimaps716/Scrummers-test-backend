a0e74e5e1b5e51006bba6dc4a2aceddbca1ceeb4
Dima <dimaps716@gmail.com>
2021-05-23 11:19:19 -0500

    fix errors in rendering

--- Scrummer/urls.py
+++ Scrummer/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('cart', include('cart.urls')),
     path('orders/', include('orders.urls')),
     path('', include('Shop.urls'))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
